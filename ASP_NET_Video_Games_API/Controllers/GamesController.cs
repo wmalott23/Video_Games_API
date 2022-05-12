4739ff65d3aed344b62973e7abab0117814d8e53
WTDula <wtdula@gmail.com>
2022-05-12 13:13:48 -0500

    finished httpget request for all video games

---
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -0,0 +1,28 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Video_Games_API.Controllers
+{
+    // /api/games
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllVideoGames()
+        {
+            var allVideoGames = _context.VideoGames.ToList();
+            return Ok(allVideoGames);
+        }
+
+
+    }
+}
